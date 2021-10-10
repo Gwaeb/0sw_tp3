60dc06083eb054fce7323c1bb0b30c2c7c54bc12
Gwaeb <gwaeb@hotmail.com>
2021-10-10 14:28:36 -0400

    input map: done

--- player.cs
+++ player.cs
@@ -8,72 +8,95 @@ public class player : KinematicBody2D
     const int MAXFALLSPEED = 200;
     const int MAXSPEED = 100;
     const int JUMPFORCE = 300;
-
     const int ACCEL = 10;
+
+
     Vector2 vZero = new Vector2();
-
-    bool facing_right = true;
-
-
     Vector2 motion = new Vector2();
 
     Sprite currentSprite;
     AnimationPlayer animPlayer;
-        // Called when the node enters the scene tree for the first time.
+    // Called when the node enters the scene tree for the first time.
+    AnimationTree animationTree;
+    AnimationNodeStateMachinePlayback animationState;
+
+    bool facing_right = true;
     public override void _Ready()
     {
         currentSprite = GetNode<Sprite>("Sprite");
         animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        animationTree = GetNode<AnimationTree>("AnimationTree");
+        animationTree.Active = true;
+        animationState = (AnimationNodeStateMachinePlayback)animationTree.Get("parameters/playback");
     }
 
     public override void _PhysicsProcess(float delta)
     {
         motion.y += GRAVITY;
 
-        if(motion.y > MAXFALLSPEED) {
+        if (motion.y > MAXFALLSPEED)
+        {
             motion.y = MAXFALLSPEED;
         }
 
-        if (facing_right) {
+        if (facing_right)
+        {
             currentSprite.FlipH = false;
-        } else {
+        }
+        else
+        {
             currentSprite.FlipH = true;
         }
 
-         motion.x = motion.Clamped(MAXSPEED).x;
+        motion.x = motion.Clamped(MAXSPEED).x;
 
-        if (Input.IsActionPressed("ui_left")) {
+        animationTree.Set("parameters/Idle/blend_position", motion);
+        animationTree.Set("parameters/Run/blend_position", motion);
+
+        if (Input.IsActionPressed("ui_left"))
+        {
             motion.x -= ACCEL;
             facing_right = false;
-            animPlayer.Play("Run");
-        } else if (Input.IsActionPressed("ui_right")) {
+            animationState.Travel("Run");
+            //animPlayer.Play("Run");
+        }
+        else if (Input.IsActionPressed("ui_right"))
+        {
             motion.x += ACCEL;
             facing_right = true;
-            animPlayer.Play("Run");
-        } else {
-            motion = motion.LinearInterpolate(Vector2.Zero, 0.2f);
-            animPlayer.Play("Idle");
+            animationState.Travel("Run");
+            //animPlayer.Play("Run");
         }
+        else
+        {
+            motion = motion.LinearInterpolate(Vector2.Zero, 0.2f);
+            animationState.Travel("Idle");
+            //animPlayer.Play("Idle");
+        }
+
+
 
         if (IsOnFloor())
             // On ne regarde qu'un seul fois et non le maintient de la touche
-            if (Input.IsActionJustPressed("ui_jump")) {
+            if (Input.IsActionJustPressed("ui_jump"))
+            {
                 motion.y = -JUMPFORCE;
                 GD.Print($"motion.y = {motion.y}");
                 Console.WriteLine($"motion.y = {motion.y}");
             }
 
-        if (!IsOnFloor()) {
-            if (motion.y < 0) {
+        if (!IsOnFloor())
+        {
+            if (motion.y < 0)
+            {
                 animPlayer.Play("jump");
-            } else if (motion.y > 0) {
+            }
+            else if (motion.y > 0)
+            {
                 animPlayer.Play("fall");
             }
         }
 
         motion = MoveAndSlide(motion, UP);
     }
-
-
-    
 }
